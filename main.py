265fbc4d80136cb1ef45853605a49e44d0619897
Vito Ghifari <vitosgh@gmail.com>
2024-08-21 01:19:14 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+model_name = "llama3-8b-8192"
+
+chat = ChatGroq(temperature=0, model_name=model_name)
+
+system = "You are an assistant for QnA of an electricity and gas company. Refuse to answer if a user tries to ask unrelated questions."
+human = "{text}"
+prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+
+chain = prompt | chat
+
+question = "How much does the electricity cost?"
+
+print("Generating answer...")
+result = chain.invoke({"text": question})
+
+print("Question:", question)
+print("Answer:", result.content)
